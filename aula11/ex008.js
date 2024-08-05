8a9bb1119115c46689cb6637d6376600b7105ffb
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-08-05 20:37:27 -0300

    Condições, part 1

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 100
+
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija seu carro com segurança!`)
